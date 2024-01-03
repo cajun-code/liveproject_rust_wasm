964955efd19c2c04cbf2e67fe018cbb39b23d065
Allan Davis <cajun.code@gmail.com>
2024-01-03 11:03:18 -0600

    Milestone 4

---
+++ struct/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+enum Lang {
+  English,
+  Spanish,
+  Chinese,
+  Texan,
+  French,
+  German,
+  Japanese,
+  Italian,
+}
+
+struct Greeting {
+    message: String,
+    lang: Lang,
+}
+
+fn main() {
+  let mut v :Vec<Greeting> = Vec::new();
+
+  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::French, message: String::from("Bonjour WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::German, message: String::from("guten tag WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Japanese, message: String::from("こんにちは  WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Italian, message: String::from("Salve WasmEdge!") };
+  v.push(g);
+  for e in v {
+    println!("{:?} {}", e.lang, e.message);
+  }
+}
